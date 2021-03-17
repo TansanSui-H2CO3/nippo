aa9324e8025b84b0624a7b3cf9b5d78525469492
TansanSui-H2CO3 <tansanwater.h2co3@gmail.com>
2021-03-18 03:23:04 +0900

    feat: serve static html files to clients

--- nippo.go
+++ nippo.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -12,13 +11,15 @@ func main() {
 	serve()
 }
 
-func handler(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(res, "Hello, world.")
+func submitHandler(res http.ResponseWriter, req *http.Request) {
+	// Called twice
+	// DB operation
 }
 
 // Ref: https://gist.github.com/niratama/6b0117c6c6f2d21b5687
 func serve() {
-	http.HandleFunc("/", handler)
+	http.Handle("/", http.FileServer(http.Dir("./static/html")))
+	http.HandleFunc("/submit.html", submitHandler)
 	listen := make(chan bool)
 	go func() {
 		<-listen
