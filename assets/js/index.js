abec57975d55a02c81ebd04360641a74a32a8fc4
TansanSui-H2CO3 <tansanwater.h2co3@gmail.com>
2021-03-22 02:58:13 +0900

    fix: add require forms

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,17 +20,20 @@ function addNewTask() {
     let new_input = document.createElement('input');
     new_input.type = 'date';
     new_input.name = 'deadline-' + id_number;
+    new_input.required = true;
     document.getElementById('new-task-' + id_number).appendChild(new_input);
 
     let new_title = document.createElement('input');
     new_title.type = 'text';
     new_title.name = 'title-' + id_number;
+    new_title.required = true;
     document.getElementById('new-task-' + id_number).appendChild(new_title);
 
     let new_textarea = document.createElement('textarea');
     new_textarea.name = 'new-task-' + id_number;
     new_textarea.cols = 50;
     new_textarea.rows = 10;
+    new_textarea.required = true;
     document.getElementById('new-task-' + id_number).appendChild(new_textarea);
 
     let new_button = document.createElement('button');
